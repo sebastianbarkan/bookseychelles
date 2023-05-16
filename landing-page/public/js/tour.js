8974bff3d4e9e9846dad5fcd3ec28ebe335061a2
sebastianbarkan <86000650+sebastianbarkan@users.noreply.github.com>
2023-05-16 13:26:38 -0400

    update slideshow bottom buttons

--- landing-page/public/js/tour.js
+++ landing-page/public/js/tour.js
@@ -1,12 +1,16 @@
-let circle = document.querySelector("circle");
 function handleclick(abc, obj) {
+    let circle = document.querySelector(".circle");
     obj.classList.replace("card", "card-hide");
-
+    circle.classList.replace("circle", "circle-hide");
     let newCard = document.getElementById(`${Number(obj.id) + 1}`);
-
+    let newCircle = document.getElementById(`${Number(circle.id) + 11}`);
     if (newCard === null) {
         document.getElementById("1").classList.replace("card-hide", "card");
+        document
+            .getElementById("11")
+            .classList.replace("circle-hide", "circle");
     } else {
         newCard.classList.replace("card-hide", "card");
+        newCircle.classList.replace("circle-hide", "circle");
     }
 }
